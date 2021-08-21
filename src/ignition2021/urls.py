165f399908e31f28adf90e08be4215d92baf9b62
Justin Lu <wabasabi359@gmail.com>
2021-08-21 12:42:17 -0400

    Set up templates and index

--- src/ignition2021/urls.py
+++ src/ignition2021/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.urls import path
+from . import views 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    path('', views.index,name="index")
 ]
