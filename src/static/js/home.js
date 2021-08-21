3a957b9b58662aceb43cba60b925b6c3ee18dbbb
KennethRuan <kennethjruan@gmail.com>
2021-08-21 18:15:49 -0400

    parallax improvements

--- src/static/js/home.js
+++ src/static/js/home.js
@@ -26,6 +26,16 @@ function castParallax() {
 			layer.setAttribute('style', 'transform: translate3d(0px, ' + yPos + 'px, 0px)');
 
 		}
+
+        var layers = document.getElementsByClassName("fix");
+		var layer, speed, yPos;
+		for (var i = 0; i < layers.length; i++) {
+			layer = layers[i];
+			speed = layer.getAttribute('data-speed');
+			var yPos = -((top-document.height/4) * speed / 100);
+			layer.setAttribute('style', 'transform: translate3d(0px, ' + yPos + 'px, 0px)');
+
+		}
 	});
 }
 
