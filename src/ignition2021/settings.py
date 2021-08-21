98c63e3d55a377fc3073aa781434018b672a660b
Justin Lu <wabasabi359@gmail.com>
2021-08-20 23:32:42 -0400

    Add previously unadded files

--- src/ignition2021/settings.py
+++ src/ignition2021/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
-import os
+import os, dotenv
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
