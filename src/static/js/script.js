ee228cefb6824b275527c35313649327e3a94ded
Justin Lu <wabasabi359@gmail.com>
2021-08-22 04:32:28 -0400

    Add name to upload, fix so that button uploads too:

--- src/static/js/script.js
+++ src/static/js/script.js
@@ -4,7 +4,7 @@ dragText = dropArea.querySelector("header"),
 button = dropArea.querySelector("button"),
 input = dropArea.querySelector("input");
 
-let file, sizeOfFile; 
+let file, sizeOfFile, nameOfFile; 
 
 function another(){
     // input.click();
@@ -13,31 +13,35 @@ function another(){
     document.getElementById("processing").style.display = "none";
     document.getElementById("complete").style.display = "none";
     document.getElementById("another").style.display = "none";
-    // for(let xx of document.getElementsByClassName("drag-area")){
-    //     // xx.style.border = "2px dashed var(--box-grey)";
-    //     xx.classList.add("used");
-    // }
     dropArea.classList.remove("used");
+    for(let xx of document.getElementsByClassName("drag-area")){
+        xx.style.removeProperty('border');
+    }
+}
+
+button.onclick = ()=>{
+    input.click();
 }
 
 input.addEventListener("change", function(){
     file = this.files[0];
     sizeOfFile = file.size;
-    dropArea.classList.add("active");
+    nameOfFile = file.name;
+    dropArea.classList.add("used");
     uploadFile();
 })
 
 //if user drag file over box
 dropArea.addEventListener("dragover", ()=>{
     event.preventDefault();
-    console.log("File is over DragArea");
+    // console.log("File is over DragArea");
     dropArea.classList.add("active");
     dragText.textContent = "Release to Upload File";
 })
 
 //if user leave drag file from box
 dropArea.addEventListener("dragleave", ()=>{
-    console.log("File is outside DragArea");
+    // console.log("File is outside DragArea");
     dropArea.classList.remove("active");
     dragText.textContent = "Drag & Drop to Upload File";
 })
@@ -48,32 +52,32 @@ dropArea.addEventListener("drop", (event)=>{
     //getting user select file and [0] this means if user select multiple files then we'll select only the first one
     file = event.dataTransfer.files[0];
     sizeOfFile = file.size;
-    document.getElementById("upload").style.display = "none";
-    document.getElementById("uploaded").style.display = "block";
-    document.getElementById("processing").style.display = "block";
-    // for(let xx of document.getElementsByClassName("drag-area")){
-    //     // xx.style.border = "2px solid var(--wine-red)";
-    //     xx.classList.add("used");
-    // }
-    document.getElementById("size-file").innerHTML = sizeOfFile.toString() + " bytes";
-    dropArea.classList.remove("active");
-    dropArea.classList.add("used");
+    nameOfFile = file.name;
     uploadFile();
 });
 
-
-
 async function uploadFile() {
+    document.getElementById("upload").style.display = "none";
+    document.getElementById("uploaded").style.display = "block";
+    document.getElementById("processing").style.display = "block";
+    for(let xx of document.getElementsByClassName("drag-area")){
+        xx.style.border = "2px solid var(--wine-red)";
+    }
+    document.getElementById("size-file").innerHTML = sizeOfFile.toString() + " bytes";
+    document.getElementById("name-file").innerHTML = nameOfFile.toString();
+    dropArea.classList.remove("active");
+    dropArea.classList.add("used");
+
     let formData = new FormData();
     formData.append("file", file);
 
     try {
         await fetch('/upload/', {method: "POST", body: formData})
         .then(response => {
-            console.log(response);
+            // console.log(response);
             response.blob()
             .then(blobResponse => {
-                console.log(blobResponse);
+                // console.log(blobResponse);
                 var url  = window.URL.createObjectURL(blobResponse);
                 window.location.assign(url);
             })
