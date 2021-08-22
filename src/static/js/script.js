dd9265e3c05a49760c9c3b305e50c12b74032740
Justin Lu <wabasabi359@gmail.com>
2021-08-22 03:31:15 -0400

    change?

--- src/static/js/script.js
+++ src/static/js/script.js
@@ -1,4 +1,3 @@
-
 //selecting all required elments
 const dropArea = document.querySelector(".drag-area"),
 dragText = dropArea.querySelector("header"),
@@ -38,70 +37,39 @@ dropArea.addEventListener("drop", (event)=>{
     //getting user select file and [0] this means if user select multiple files then we'll select only the first one
     file = event.dataTransfer.files[0];
     
-
-    var c = document.getElementById("myCanvas");
-    var ctx = c.getContext("2d");
-    ctx.beginPath();
-    ctx.rect(50, 20, 200, 150);
-    ctx.fill();
     document.getElementById("upload").style.display = "none";
     document.getElementById("uploaded").style.display = "block";
-    document.getElementById("upload-button").style.display = "block";
-
+    document.getElementById("processing").style.display = "block";
+    for(let xx of document.getElementsByClassName("drag-area")){
+        xx.style.border = "2px solid var(--wine-red)";
+    }
     uploadFile();
 });
 
-// function showFile(){
-//     let fileType = file.type;
-//     let fileName = file.name;
-//     let fileSize = file.size;
-//     console.log(fileName);
-//     let validExtensions = ["application/vnd.openxmlformats-officedocument.presentationml.presentation", "image/jpeg", "image/jpg"];
-//     if(validExtensions.includes(fileType)){
-//         let fileReader = new FileReader(); // creating new filereader object
-//         fileReader.onload = ()=>{
-//             var c = document.getElementById("myCanvas");
-//             var ctx = c.getContext("2d");
-//             ctx.beginPath();
-//             ctx.rect(50, 20, 200, 150);
-//             ctx.fill();
-//             document.getElementById("upload").style.display = "none";
-//             document.getElementById("uploaded").style.display = "block";
-//             document.getElementById("boots").style.display = "block";
-//         }
-//         fileReader.readAsDataURL(file);
-//     }
-//     else{
-//         alert("This is not an Image File!");
-//         dropArea.classList.remove("active");
-//     }
-// }
-
-const toBase64 = file => new Promise((resolve, reject) => {
-    const reader = new FileReader();
-    reader.readAsDataURL(file);
+function showFile(){
+    let fileType = file.type;
+    let fileName = file.name;
+    let fileSize = file.size;
+    console.log(fileName);
     let validExtensions = ["application/vnd.openxmlformats-officedocument.presentationml.presentation", "image/jpeg", "image/jpg"];
-
-    reader.onload = () => {
-        var c = document.getElementById("myCanvas");
-        var ctx = c.getContext("2d");
-        ctx.beginPath();
-        ctx.rect(50, 20, 200, 150);
-        ctx.fill();
-        document.getElementById("upload").style.display = "none";
-        document.getElementById("uploaded").style.display = "block";
-        document.getElementById("upload-button").style.display = "block";
-        resolve(reader.result);
-    };
-    reader.onerror = error => reject(error);
-});
-
-async function showFile() {
-    console.log("started");
-    let result = await toBase64(file);
-    console.log(result);
-    console.log("finished");
-    newWindow = window.open(result, 'newDocument');
+    if(validExtensions.includes(fileType)){
+        let fileReader = new FileReader(); // creating new filereader object
+        fileReader.onload = ()=>{
+            // var c = document.getElementById("myCanvas");
+            // var ctx = c.getContext("2d");
+            // ctx.beginPath();
+            // ctx.rect(50, 20, 200, 150);
+            // ctx.fill();
+            // document.getElementById("upload").style.display = "none";
+            // document.getElementById("uploaded").style.display = "block";
+            // document.getElementById("boots").style.display = "block";
+        }
+        fileReader.readAsDataURL(file);
+    }
+    else{
+        alert("This is not an Image File!");
+        dropArea.classList.remove("active");
+    }
 }
 
 async function uploadFile() {
@@ -111,16 +79,17 @@ async function uploadFile() {
     try {
         await fetch('/upload/', {method: "POST", body: formData})
         .then(response => {
-                console.log(response);
-                response.blob()
-                .then(blobResponse => {
-                    console.log(blobResponse);
-                    var url  = window.URL.createObjectURL(blobResponse);
-                    window.location.assign(url);
-                })
-          });
-          console.log("Done");
-          fetch('/delete/', {method: "GET", body: null});
+            console.log(response);
+            response.blob()
+            .then(blobResponse => {
+                console.log(blobResponse);
+                var url  = window.URL.createObjectURL(blobResponse);
+                window.location.assign(url);
+            })
+        });
+        document.getElementById("processing").style.display = "none";
+        document.getElementById("complete").style.display = "block";
+        fetch('/delete/', {method: "GET", body: null});
         // console.log('HTTP response code:',r.status);
      } catch(e) {
         console.log('Error while uploading data: ', e);
